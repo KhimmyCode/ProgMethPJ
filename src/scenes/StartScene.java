6f8fe670e1f904512789838eb24292cd2fa35d11
Peerav <newpeeravit@gmail.com>
2025-03-06 22:56:05 +0700

    addgamename

--- src/scenes/StartScene.java
+++ src/scenes/StartScene.java
@@ -13,6 +13,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.text.Font;
 import application.SceneManager;
 
@@ -37,19 +38,25 @@ public class StartScene {
 
         // "Press to Start" Text
         Text title = new Text("Press to Start");
+        Text gamename = new Text(150,270,"KingDom \nTowerDefense");
         SceneManager s = new SceneManager();
-        Font pixelFont = s.loadFont("/fonts/pixel2.ttf",65);
+        Font pixelFont = s.loadFont("/fonts/pixel2.ttf",45);
+        Font pixelFont2 = s.loadFont("/fonts/pixel2.ttf",80);
         title.setFont(pixelFont); 
+        gamename.setFont(pixelFont2);
+        gamename.setFill(Color.DARKRED); // Text Color
+        gamename.setStroke(Color.ROSYBROWN); // Outline color
+        gamename.setTextAlignment(TextAlignment.CENTER);
         title.setFill(Color.BLACK); // Text Color
         title.setStroke(Color.WHITE); // Outline color
         title.setStrokeWidth(1);
-        title.setX(150);
+        title.setX(230);
         title.setY(500);
 
         // Click Anywhere to Start
         root.setOnMouseClicked((MouseEvent e) -> SceneManager.setScene("level"));
 
-        root.getChildren().addAll (image,title);
+        root.getChildren().addAll (image,title,gamename);
         scene = new Scene(root, 800, 600);
     }
 
